a618043676666358a830a2b0af1ed19815dffe6f
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-06-05 17:53:45 -0300

    adicionando chao na sala principal e tentando melhora pos processamento

--- Terror/Assets/Script/LuzPosProcessamento.cs
+++ Terror/Assets/Script/LuzPosProcessamento.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class LusPosProcessamento : MonoBehaviour,ILuz
+public class LuzPosProcessamento : MonoBehaviour,ILuz
 {
     [Header("Pos Processamento")]
     [SerializeField] private Volume volume;
@@ -12,10 +12,17 @@ public class LusPosProcessamento : MonoBehaviour,ILuz
     private float bloomOriginal;
     private Bloom bloom;
 
-    [Header("Exposure")]
+    [Header("Exposure aumento")]
     [SerializeField] private float exposureAumentado = 1.5f;
     private float exposureOriginal;
     private ColorAdjustments colorAdjustments;
+
+    [Header(" Shadows Midtones Highlights aumento")]
+    [SerializeField] private Vector4 shadowsAumentado = new Vector4(1.1f, 1.1f, 1.1f, 0f);
+    [SerializeField] private Vector4 midtonesAumentado = new Vector4(1.2f, 1.2f, 1.2f, 0f);
+    private Vector4 shadowsOriginal;
+    private Vector4 midtonesOriginal;
+    private ShadowsMidtonesHighlights smh;
 
     private void Start()
     {
@@ -30,6 +37,16 @@ public class LusPosProcessamento : MonoBehaviour,ILuz
             exposureOriginal = colorAdjustments.postExposure.value;
             colorAdjustments.postExposure.value = exposureAumentado;
         }
+
+        if (volume.profile.TryGet(out smh))
+        {
+            shadowsOriginal = smh.shadows.value;
+             midtonesOriginal = smh.midtones.value;
+
+            smh.shadows.value = shadowsAumentado;
+            smh.midtones.value = midtonesAumentado;
+            
+        }
     }
 
     public void AtivarEfeito()
@@ -39,6 +56,13 @@ public class LusPosProcessamento : MonoBehaviour,ILuz
 
         if (colorAdjustments != null)
             colorAdjustments.postExposure.value = exposureAumentado;
+
+        if (smh != null)
+        {
+            smh.shadows.value = shadowsAumentado;
+            smh.midtones.value = midtonesAumentado;
+         
+        }
     }
 
     public void RestaurarEfeito()
@@ -48,14 +72,21 @@ public class LusPosProcessamento : MonoBehaviour,ILuz
 
         if (colorAdjustments != null)
             colorAdjustments.postExposure.value = exposureOriginal;
+
+        if (smh != null)
+        {
+            smh.shadows.value = shadowsOriginal;
+            smh.midtones.value = midtonesOriginal;
+           
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Lixo"))
         {
-            RestaurarEfeito(); 
-            
+            RestaurarEfeito();
+           
         }
     }
 }
