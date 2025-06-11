a5bc17f4148b4e6b93353cc662716db2ec8f8ecb
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-06-10 21:54:46 -0300

    façando a primeira animaçao de jumpsCare

--- Terror/Assets/Script/Animatronics.cs
+++ Terror/Assets/Script/Animatronics.cs
@@ -1,87 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+public enum AnimatronicsStatus
+{
+    Wait,Patrol,Chase,Search
+}
 
 public class Animatronics : MonoBehaviour,IAnimatronic
 {
-    [Header("Animaçao")]
-    [SerializeField] private Animator animator;
-
     [Header("NavMesh")]
-    [SerializeField] private NavMeshAgent agente;
+    NavMeshAgent agent;
 
     [Header("Player")]
-    [SerializeField] private Transform jogador;
+    [SerializeField] Transform player;
 
-    [Header("Patrulha")]
-    [SerializeField] private Transform[] pontosPatrulha;
+    [Header("Pontos Patrulha")]
+    [SerializeField] Transform[] patrolPoints;
+
+    [Header("Animator")]
+    [SerializeField] Animator animator;
+
+    [Header("GameOver")]
+    [SerializeField] string cenaGameOver = "GameOver";
+
+    [Header("WaitTime")]
+    [SerializeField] float waitTime = 2f;
 
     [Header("Jumpscare")]
-    [SerializeField] private float distanciaJumpscare = 3f;
-    [SerializeField] private float tempoJumpscare = 2f;
+    [SerializeField] float distanciaJumpscare = 3f;
 
-    [Header("Cena Game Over")]
-    [SerializeField] private string cenaGameOver = "GameOver";
-   
+    [Header("Timeline")]
+    [SerializeField] PlayableDirector jumpscareTimeline;
 
-    private int indexPontoAtual = 0;
-    private bool patrulhando = false;
+    [Header("Câmeras")]
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject jumpscareCamera;
+
+    private AnimatronicsStatus state;
     private bool emJumpscare = false;
 
-
-  private void Start()
-  {
-    agente.enabled = false;
-    animator.SetTrigger("Dancar");
-
-   
-    if (ControladorAnimatronic.instancia != null)
+    void Start()
     {
-        ControladorAnimatronic.instancia.Registrar(this);
-    }
-  }
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+        animator.SetTrigger("Dancar");
 
-    private void Update()
-    {
+        if (ControladorAnimatronic.instancia != null)
+            ControladorAnimatronic.instancia.Registrar(this);
+
         
-        if (!patrulhando || emJumpscare) return;
-         
+        if (mainCamera != null) mainCamera.SetActive(true);
+        if (jumpscareCamera != null) jumpscareCamera.SetActive(false);
+    }
 
-        float distancia = Vector3.Distance(transform.position, jogador.position);
+    void Update()
+    {
+        if (emJumpscare || !agent.enabled)
+            return;
+
+        float distancia = Vector3.Distance(transform.position, player.position);
+
         if (distancia <= distanciaJumpscare)
         {
             IniciarJumpscare();
             return;
         }
 
-        if (!agente.pathPending && agente.remainingDistance < 0.5f)
+        Looking();
+
+        switch (state)
         {
-            indexPontoAtual = (indexPontoAtual + 1) % pontosPatrulha.Length;
-            agente.SetDestination(pontosPatrulha[indexPontoAtual].position);
+            case AnimatronicsStatus.Wait:
+                break;
+
+            case AnimatronicsStatus.Patrol:
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                    SetState(AnimatronicsStatus.Wait);
+                break;
+
+            case AnimatronicsStatus.Chase:
+                agent.SetDestination(player.position);
+                break;
+
+            case AnimatronicsStatus.Search:
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                    SetState(AnimatronicsStatus.Wait);
+                break;
         }
+    }
+
+    public void SetState(AnimatronicsStatus newState)
+    {
+        switch (newState)
+        {
+            case AnimatronicsStatus.Wait:
+                animator.SetTrigger("Parar");
+                StartCoroutine(Waiting());
+                break;
+            case AnimatronicsStatus.Patrol:
+                animator.SetTrigger("Andar");
+                agent.SetDestination(patrolPoints[Random.Range(0, patrolPoints.Length)].position);
+                break;
+            case AnimatronicsStatus.Chase:
+                animator.SetTrigger("Correr");
+                break;
+            case AnimatronicsStatus.Search:
+                animator.SetTrigger("Andar");
+                agent.SetDestination(patrolPoints[Random.Range(0, patrolPoints.Length)].position);
+                break;
+        }
+
+        state = newState;
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SetState(AnimatronicsStatus.Patrol);
+    }
+
+    public void Looking()
+    {
+        if (Physics.Linecast(transform.position, player.position, out RaycastHit hit))
+        {
+            if (hit.transform == player)
+            {
+                if (state != AnimatronicsStatus.Chase)
+                    SetState(AnimatronicsStatus.Chase);
+            }
+            else if (state == AnimatronicsStatus.Chase)
+            {
+                SetState(AnimatronicsStatus.Search);
+            }
+        }
+    }
+
+    public void IniciarJumpscare()
+    {
+        if (emJumpscare) return;
+
+        emJumpscare = true;
+        agent.isStopped = true;
+        animator.SetTrigger("Jumpscare");
+
+       
+        if (mainCamera != null) mainCamera.SetActive(false);
+        if (jumpscareCamera != null) jumpscareCamera.SetActive(true);
+
+        
+        if (jumpscareTimeline != null)
+        {
+            jumpscareTimeline.Play();
+            StartCoroutine(EsperarTimeline(jumpscareTimeline.duration));
+        }
+        else
+        {
+            
+            Invoke(nameof(CarregarCenaGameOver), 2f);
+        }
+    }
+
+    IEnumerator EsperarTimeline(double duracao)
+    {
+        yield return new WaitForSeconds((float)duracao);
+        CarregarCenaGameOver();
+    }
+
+    void CarregarCenaGameOver()
+    {
+        SceneManager.LoadScene(cenaGameOver);
     }
 
     public void AtivarPatrulha()
     {
-       
-        patrulhando = true;
+        agent.enabled = true;
+        agent.isStopped = false;
         animator.SetTrigger("Andar");
-        agente.enabled = true;
-        agente.SetDestination(pontosPatrulha[indexPontoAtual].position);
-    }
-
-    private void IniciarJumpscare()
-    {
-        emJumpscare = true;
-        agente.isStopped = true;
-        animator.SetTrigger("Jumpscare");
-
-        
-        Invoke(nameof(CarregarGameOver), 2f);
-    }
-
-    private void CarregarGameOver()
-    {
-        SceneManager.LoadScene(cenaGameOver);
+        SetState(AnimatronicsStatus.Patrol);
     }
 }
